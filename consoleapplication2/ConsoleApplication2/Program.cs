a7f8f32fc574f47d40917e5c5b68f89a47a477ec
k1shor <k1shor@hotmail.com>
2016-07-22 01:29:58 +0545

    modified

--- consoleapplication2/ConsoleApplication2/Program.cs
+++ consoleapplication2/ConsoleApplication2/Program.cs
@@ -2,9 +2,6 @@
 using LFAForumClassLib;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
